6c5bccf0a0f9746a71abb35e1ede6729dd6bbce5
m <139261114+mauriciorfdev@users.noreply.github.com>
2024-02-25 17:25:19 -0300

    enviar respuesta: archivo index.html

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 
